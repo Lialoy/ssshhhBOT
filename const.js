a6024adc1ec5a4556c91737a767133c50d612db5
Lialoy <62553435+Lialoy@users.noreply.github.com>
2021-12-12 13:58:14 +0200

    simple bot update

--- const.js
+++ const.js
@@ -1,14 +1,16 @@
 const commands = `
 /start - restart bot
 /help - help
-/sm - social media
-`
+/credits - bot credits
+`// /help commands
+
 const text1 = `
 there is my <a href="https://instagram.com/lialoy4k19/"> Instagram </a>
 `
 const text2 = `
-That is some punk image and link to <a href="https://youtube.com/"> YouTube </a>
-`
+Author's <a href="https://t.me/lialoy/"> Telegram </a>
+` // Text for buttons
+
 module.exports.commands = commands
 module.exports.text1 = text1
-module.exports.text2 = text2
+module.exports.text2 = text2 // Export commands
--- index.js
+++ index.js
@@ -2,24 +2,30 @@ const {Telegraf, Markup} = require('telegraf')
 require('dotenv').config()
 const text = require('./const')
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
-bot.start((ctx) => ctx.reply(`Hey, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'gay'}!`))
-bot.help((ctx) => ctx.reply(text.commands))
-bot.command('sm', async(ctx)=> {
-    try{
-        await ctx.replyWithHTML('<i>Just a punks for test</i>', Markup.inlineKeyboard( //await штука чтобы подождать выполнения фрагмента кода, для сохранения последовательности
-            [
-                [Markup.button.callback('Inst', 'btn_1'), Markup.button.callback('YT', 'btn_2')]//массив с кнопкой, один массив на одн ряд кнопок
-            ]
-
-        ))
-    } catch (e) {
-    console.error
+const sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database(':memory:', (err)=> {
+    if(err){
+        return console.error(err.message)
     }
-})
+    console.log('Connected to in-memory SQlite database.')
+})  // SQLite3 database connecting with error catching
+
+bot.start((ctx) => ctx.reply(`Hey, ${ctx.message.from.first_name ? ctx.message.from.first_name : '*unknown person*'}!`)) // Welcome message with user first_name
+bot.help((ctx) => ctx.reply(text.commands)) // Help command with text from const.js commands constant
+bot.command('credits', async(ctx)=>{ 
+    try{
+        await ctx.replyWithHTML(
+            '<strong> dev.by.Lialoy </strong>', Markup.inlineKeyboard([
+                [Markup.button.callback('Instagram', 'btn_1'), Markup.button.callback('Telegram', 'btn_2')]
+            ]
+            )
+        )
+    }catch(e){
+    console.error}
+})  // Simple async command with button markup and error catching
 
 function addActionBot(name, src, text) {
-    bot.action(name, async(ctx)=> {
+    bot.action(name, async(ctx)=>{
         try{
             await ctx.answerCbQuery()
             if(src !== false){
@@ -30,15 +36,16 @@ function addActionBot(name, src, text) {
             await ctx.replyWithHTML(text,{
                 disable_web_page_preview: true
             })
-        } catch(e) {
-        console.error
+        }catch(e){
+            console.error
         }
     })
-}
+}   // Reply function with text/photo reply things and error catching
 
-addActionBot('btn_1', './img/1.jpg', text.text1)
-addActionBot('btn_2', './img/2.jpg', text.text2)
+addActionBot('btn_1', false, text.text1)
+addActionBot('btn_2', false, text.text2) // Callback data buttons
 
 bot.launch()
+console.log('by Lialoy')
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
